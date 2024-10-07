e74b3ae17d648cf23d298fef682f6f0a285c2743
Camille Marion <cam.marion14@gmail.com>
2024-10-07 19:31:48 -0400

    SASS - En-tête

--- public/liaisons/fragments/entete.inc.php
+++ public/liaisons/fragments/entete.inc.php
@@ -1,7 +1,7 @@
 <header class="entete">
-    <img src="../public/liaisons/images/logo.svg" alt="Logo">
     <nav class="menu menu--ferme">
         <ul class="menu__liste">
+            <img class="logo" src="../public/liaisons/images/logo.svg" alt="Logo">
             <li class="menu__listeItem"><a href="<?php echo $niveau; ?>index.php" class="menu__lien">Le OFF</a></li>
             <li class="menu__listeItem"><a href="<?php echo $niveau; ?>public/programmation/index.php"
                     class="menu__lien">Programmation</a></li>
@@ -9,8 +9,8 @@
                     class="menu__lien">Artistes</a></li>
             <li class="menu__listeItem"><a href="<?php echo $niveau; ?>public/lieux/index.php"
                     class="menu__lien">Partenaires</a></li>
-            <a><img src="../public/liaisons/images/sac.svg" alt=""></a>
-            <a><img src="../public/liaisons/images/loupe.svg" alt=""></a>
+            <a class="sac"><img src="../public/liaisons/images/sac.svg" alt=""></a>
+            <a class="loupe"><img src="../public/liaisons/images/loupe.svg" alt=""></a>
             <button type="">Billets</button>
         </ul>
     </nav>
