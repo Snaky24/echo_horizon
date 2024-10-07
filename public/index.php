d2d9d41d23ae2fb17168e397251d79e8e29e9377
Camille Marion <cam.marion14@gmail.com>
2024-10-07 15:10:49 -0400

    Liens

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
-
-<?php $niveau="../";?>
-<?php include ($niveau . "public/liaisons/php/config.inc.php");?>
+<?php $niveau = "../"; ?>
+<?php include($niveau . "public/liaisons/php/config.inc.php"); ?>
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="">
@@ -11,24 +11,24 @@
 	<meta name="author" content="">
 	<meta charset="utf-8">
 	<title>Un beau titre ici!</title>
-	<?php include ($niveau . "public/liaisons/fragments/headlinks.inc.php");?>
+	<?php include($niveau . "public/liaisons/fragments/headlinks.inc.php"); ?>
 </head>
 
 <body>
-
-	<?php include ($niveau . "public/liaisons/fragments/entete.inc.php");?>
+	<?php $niveau = "../"; ?>
+	<?php include($niveau . "public/liaisons/fragments/entete.inc.php"); ?>
 
 	<main>
 		<div id="contenu" class="conteneur">
 			<h2>Entête de page</h2>
 
 			<?php
-			$requeteSQL="Select titre from actualites";
-			$objStat=$objPdo -> prepare($requeteSQL);
-			$objStat -> execute();
-			$arrActualite=$objStat -> fetchAll();
-			forEach($arrActualite as $actualite){
-				echo $actualite["titre"];?><BR>
+			$requeteSQL = "Select titre from actualites";
+			$objStat = $objPdo->prepare($requeteSQL);
+			$objStat->execute();
+			$arrActualite = $objStat->fetchAll();
+			foreach ($arrActualite as $actualite) {
+				echo $actualite["titre"]; ?><BR>
 			<?php } ?>
 
 			<section>
@@ -37,8 +37,10 @@
 					<header>
 						<h4>Entête d'article</h4>
 					</header>
-					<p>Lorem ipsum dolor HTML5 nunc aut nunquam sit amet, consectetur adipiscing elit. Vivamus at est eros, vel fringilla urna.</p>
-					<p>Per inceptos himenaeos. Quisque feugiat, justo at vehicula pellentesque, turpis lorem dictum nunc.</p>
+					<p>Lorem ipsum dolor HTML5 nunc aut nunquam sit amet, consectetur adipiscing elit. Vivamus at est
+						eros, vel fringilla urna.</p>
+					<p>Per inceptos himenaeos. Quisque feugiat, justo at vehicula pellentesque, turpis lorem dictum
+						nunc.</p>
 					<footer>
 						<h5>Pied d'article</h5>
 					</footer>
@@ -47,27 +49,30 @@
 					<header>
 						<h4>Entête d'article</h4>
 					</header>
-					<p>Lorem ipsum dolor nunc aut nunquam sit amet, consectetur adipiscing elit. Vivamus at est eros, vel fringilla urna. Pellentesque odio</p>
+					<p>Lorem ipsum dolor nunc aut nunquam sit amet, consectetur adipiscing elit. Vivamus at est eros,
+						vel fringilla urna. Pellentesque odio</p>
 					<footer>
 						<h5>Pied d'article</h5>
 					</footer>
 				</article>
 			</section>
 		</div>
-	
-   
-        <p><a href="#" class="bouton">Bouton</a></p>
+
+
+		<p><a href="#" class="bouton">Bouton</a></p>
 		<p><a href="#" class="bouton--inverse">Bouton</a></p>
-     <a href="#" class="hyperlien">lien test!</a>
+		<a href="#" class="hyperlien">lien test!</a>
 	</main>
-	
+
 	<aside>
-            <h3>Barre latérale</h3>
-            <p>Lorem ipsum dolor nunc aut nunquam sit amet, consectetur adipiscing elit. Vivamus at est eros, vel fringilla urna. Pellentesque odio rhoncus</p>
+		<h3>Barre latérale</h3>
+		<p>Lorem ipsum dolor nunc aut nunquam sit amet, consectetur adipiscing elit. Vivamus at est eros, vel fringilla
+			urna. Pellentesque odio rhoncus</p>
 	</aside>
-	
-	
-	<?php include ($niveau . "liaisons/fragments/piedDePage.inc.php");?>
+
+
+	<?php include($niveau . "liaisons/fragments/piedDePage.inc.php"); ?>
 
 </body>
+
 </html>
