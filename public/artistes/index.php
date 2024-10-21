e141c5811af119117b49db9e2890eaa8e34d6bfa
Elma Atindehou <2233755@csfoy.ca>
2024-10-21 13:59:57 -0400

    premier commit

--- public/artistes/index.php
+++ public/artistes/index.php
@@ -2,4 +2,30 @@ Artistes!
 <?php $niveau="../";?>
 <a href="<?php echo $niveau;?>index.php">Retour</a>
 
-<?php include ($niveau . "public/liaisons/fragments/headlinks.inc.php");?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- NE PAS OUBLIER LE RESTE DES META -->
+    <title>Document</title>
+</head>
+<body>
+<!-- À faire par Camille -->
+    <header></header>
+    
+    <main class="informationsPrincipales">
+        <select name="filtresProgrammtion" id="filtresProgrammtion">
+            <option value="parDefaut">Par défaut</option>
+            <option value="date">Date</option>
+            <option value=""></option>
+        </select>
+    </main>
+
+
+<!-- À faire par Camille -->
+    <footer></footer>
+</body>
+</html>
