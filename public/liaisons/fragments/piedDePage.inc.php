0ab286a753b1f7c4d0e430a58e3d8699a77fccae
Jade Mayrand <1942810@csfoy.ca>
2024-11-04 13:39:30 -0500

    resolu bug

--- public/liaisons/fragments/piedDePage.inc.php
+++ public/liaisons/fragments/piedDePage.inc.php
@@ -1,18 +1,3 @@
-<?php $niveau = ""; ?>
-<?php include($niveau . "liaisons/php/config.inc.php"); ?>
-
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="keyword" content="">
-    <meta name="author" content="">
-    <meta charset="utf-8">
-    <title>Un beau titre ici!</title>
-    <?php include($niveau . "liaisons/fragments/headlinks.inc.php"); ?>
-</head>
 
 <footer class="piedDePage">
     <hr class="separator">
